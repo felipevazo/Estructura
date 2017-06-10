8f84ba81c4b46ca67ef4addaaa680430c0d057fb
DEBOCERRARGITHUB BY MATIGUTIERREZ CS2 <f.vasquez07@ufromail.cl>
2017-06-10 17:29:32 -0400

    Create TemperatureItem.cpp

---
+++ TemperatureItem.cpp
@@ -0,0 +1,22 @@
+include <iostream>
+using namespace std;
+class TemperatureItem: public SensorItem()
+{
+  public:
+  {
+    TemperatureItem(data):SensorItem();
+
+    void printData
+  {
+    cout<<data
+  }
+    void getData
+    {
+      return data;
+    }
+  };
+  protected:
+  {
+   float data;
+}
+};
