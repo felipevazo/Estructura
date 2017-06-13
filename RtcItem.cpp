d6448d8cb8d23a4123e662538f024341c3c3acb4
DEBOCERRARGITHUB BY MATIGUTIERREZ CS2 <f.vasquez07@ufromail.cl>
2017-06-13 14:57:44 -0400

    Create RtcItem.cpp

--- RtcItem.cpp
+++ RtcItem.cpp
@@ -1,1 +1,22 @@
-
+include <iostream>
+using namespace std;
+class RtcItem:public SensorItem()
+{
+  public:
+  
+    RtcItem(data):SensorItem()
+    
+    void print()
+    {
+      cout<<data;
+    }
+    time_t getData()
+    {
+      return data;
+    }
+  
+  protected:
+  
+    time_t data;
+  
+};
