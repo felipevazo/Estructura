1e78790c5d07e2dfcd5ce73238d7cfb52e8e582e
DEBOCERRARGITHUB BY MATIGUTIERREZ CS2 <f.vasquez07@ufromail.cl>
2017-06-13 14:47:00 -0400

    Create WeightItem.cpp

---
+++ WeightItem.cpp
@@ -0,0 +1,17 @@
+include <iostream>
+using namespace std;
+class DatoM: public SensorItem()
+{
+  public:
+  WeightItem(data):SensorItem();
+    void print
+  {
+      
+    cout<<data
+  }
+   long getData
+  protected:
+  {
+   long data;
+  }
+}
