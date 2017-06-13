6d2bbf361d2c08ff27e0217cf5b861f18ff92e34
DEBOCERRARGITHUB BY MATIGUTIERREZ CS2 <f.vasquez07@ufromail.cl>
2017-06-13 15:07:20 -0400

    Create ImuItem.cpp

--- ImuItem.cpp
+++ ImuItem.cpp
@@ -2,3 +2,25 @@ include <iostream>
 using namespace std;
 class ImuItem:public SensorItem()
 {
+  public:
+  {
+    ImuItem(datax,datay,dataz):SensorItem();
+    void print()
+    {
+      cout<<"("+datax+","+datay+","+dataz);  
+    }
+    vector<int> getData()
+    {
+      int vector[3]={datax,datay,dataz};
+    }
+  }
+  
+  
+  
+  protected:
+  {
+    int datax;
+    int datay;
+    int dataz;
+  }
+}
